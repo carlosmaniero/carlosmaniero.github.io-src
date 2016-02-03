2bae6bc31bb517822adff9ec337024cd47f4df1a
Carlos Maniero <carlosmaniero@gmail.com>
2016-02-03 16:52:35 -0200

    Hello World Post

---
+++ content/helloworld.md
@@ -0,0 +1,7 @@
+Title: Hello World
+Date: 2016-02-03 16:09
+Category: Diary
+
+This is my first post. I've posted on my [http://carlosmaniero.blogspot.com](Blogspot)[pt], but now I will migrate my blog to this plataform.
+
+Now, I use [http://blog.getpelican.com/](Pelican), an amazing static website generator, powered by Python <3.
---
+++ pelicanconf.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Carlos Maniero'
+SITENAME = u'Dev Diary'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+LINKS = []
+
+# Social widget
+SOCIAL = (
+    ('GitHub', 'http://github.com/carlosmaniero'),
+    ('Facebook', 'http://facebook.com/carlosmaniero'),
+    ('Twitter', 'http://twitter.com/carlosmaniero'),
+)
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'themes/Casper2Pelican'
+
+# Theme Configs
+AUTHOR_PIC_URL = 'https://avatars0.githubusercontent.com/u/2002011?v=3&s=460'
+AUTHOR_BIO = 'Python Developer and an open source evangelist. ' \
+             'Focused in web  development. ' \
+             'Working with Django and Tornado for async web solutions. ' \
+             'A lover of the Linux terminal and the creator of the Pygamii, ' \
+             'a ascii game engine written in Python.'
+AUTHOR_LOCATION = 'São Paulo/Brazil'
+TWITTER_USERNAME = 'carlosmaniero'
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
